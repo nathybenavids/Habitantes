ab3566913b3b3d39c47e10bb1a35f0a8c96352ad
nathybenavids <73511871+nathybenavids@users.noreply.github.com>
2021-06-23 01:25:45 -0500

    Add files via upload

---
+++ Pais.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_2
+{
+    class Pais
+    {
+        public int PaisId
+        {
+            get; set;
+        }
+        public string Nombre
+        {
+            get; set;
+        }
+
+        public List<Provincia> Provincias
+        {
+            get; set;
+        } = new List<Provincia>();
+
+        public int ObtenerNumeroDeHabitantes()
+        {
+            int acumulador = 0;
+
+            foreach (Provincia actual in Provincias)
+            {
+                acumulador = acumulador + actual.ObtenerNumeroDeHabitantes();
+            }
+            return acumulador;
+        }
+    }
+}
---
+++ Parroquia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_2
+{
+    class Parroquia
+    {
+        public int ParroquiaId
+        {
+            get; set;
+        }
+        public string Nombre
+        {
+            get; set;
+        }
+        public int Habitantes
+        {
+            get; set;
+        }
+    }
+
+
+}
